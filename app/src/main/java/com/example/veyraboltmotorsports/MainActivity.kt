c137560e6e59dc7264260b20212fa5bc61b0b2a3
Michael S. Galozo <galozomichael@gmail.com>
2026-05-06 09:55:22 +0800

    feat(ui): add activity_checkout layout and payment backgrounds

--- app/src/main/java/com/example/veyraboltmotorsports/MainActivity.kt
+++ app/src/main/java/com/example/veyraboltmotorsports/MainActivity.kt
@@ -9,6 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_itemshop)
+        setContentView(R.layout.activity_checkout)
     }
 }
