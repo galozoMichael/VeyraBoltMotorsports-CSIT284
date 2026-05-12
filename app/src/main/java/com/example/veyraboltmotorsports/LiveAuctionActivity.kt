8823e495100585b0f6bc1a19e13e1d9a9fdb3edd
Michael S. Galozo <galozomichael@gmail.com>
2026-05-12 23:20:05 +0800

    fixing everything buh 💀💀

--- app/src/main/java/com/example/veyraboltmotorsports/LiveAuctionActivity.kt
+++ app/src/main/java/com/example/veyraboltmotorsports/LiveAuctionActivity.kt
@@ -9,12 +9,5 @@ import androidx.core.view.WindowInsetsCompat
 class LiveAuctionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_live_auction)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
